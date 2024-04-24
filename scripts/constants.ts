3c3aaf307a24fb1d30474fffae45efab9b47ac77
Amir <anasvsfs@gmail.com>
2024-04-24 16:33:17 +0900

    update configs

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -4,8 +4,8 @@ import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 /* Constants for RPC Connection the Solana Blockchain */
 export const commitmentLevel = "confirmed";
 export const endpoint =
-  process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL ||
-  "https://wider-alien-shadow.solana-mainnet.quiknode.pro/2791b2f04f16035d8a6d260466afe20d4cefde2a/";
+  process.env.RPC_URL ||
+  "https://api.mainnet-beta.solana.com";
 export const connection = new Connection(endpoint, commitmentLevel);
 
 /* Constants for the Deployed "Hello World" Program */
